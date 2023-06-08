9bf12a58d3e17c7a0e0f86918b47f15ed32861ea
mohamedkhaled68 <mohamed.khaled8ooo8@gmail.com>
2023-06-08 16:51:45 +0300

    services sec on home page

--- src/components/Home/ServicesCard.js
+++ src/components/Home/ServicesCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ServicesCard = ({ category, description, imgUrl }) => {
     return (
-        <div className="flex flex-col items-center p-4 py-10 gap-8 hover:shadow-lg duration-300">
+        <div className="flex flex-col items-center p-4 py-10 gap-8 hover:shadow-lg duration-300 hover:text-primary duration-700">
             <img src={imgUrl} alt="/" className='w-24'/>
             <h1 className='text-[1.7rem]'>{category}</h1>
             <p className='text-center text-[1.13rem]'>{description}</p>

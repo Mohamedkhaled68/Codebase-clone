8af9fccb925a933f5322cce40df2495e454f23a5
Mohamedkhaled68 <mohamed.khaled8ooo8@gmail.com>
2023-07-04 06:34:38 +0300

    work sec is done

--- src/components/Work/Gallary.js
+++ src/components/Work/Gallary.js
@@ -1,13 +1,77 @@
 import React from "react";
 import { workGallary } from "../../data/WorkGallary";
+import { useState } from "react";
 
 const Gallary = () => {
+    const [gallary, setGallary] = useState(workGallary);
+    const [selection, setSelection] = useState("All");
+    const categories = ["All", "Websites", "Mobile Applications", "Branding"];
+    const filterImages = (category) => {
+        categories.map((cate) => {
+            if (category === cate) {
+                if (cate == "All") {
+                    setSelection("All");
+                    setGallary(workGallary);
+                } else {
+                    setGallary(
+                        workGallary.filter((slide) =>
+                            slide.type.includes(category)
+                        )
+                    );
+                    setSelection(category);
+                }
+            }
+        });
+
+        // old version of filter function ==>>
+
+        // if (category === "All") {
+        //     setGallary(workGallary);
+        //     setSelection(category);
+        // } else if (category === "Websites") {
+        //     setGallary(
+        //         workGallary.filter((slide) => slide.type.includes(category))
+        //     );
+        //     setSelection(category);
+        // } else if (category === "Mobile Applications") {
+        //     setGallary(
+        //         workGallary.filter((slide) => slide.type.includes(category))
+        //     );
+        //     setSelection(category);
+        // } else if (category === "Branding") {
+        //     setGallary(
+        //         workGallary.filter((slide) => slide.type.includes(category))
+        //     );
+        //     setSelection(category);
+        // } else {
+        //     setGallary(workGallary);
+        //     setSelection("All");
+        // }
+    };
     return (
+        <>
+            <div className="container mx-auto flex justify-center items-center gap-[0.1rem] mb-[4rem] md:gap-[1rem] lg:gap-[4rem]">
+                {categories.map((category, idx) => {
+                    return (
+                        <button
+                            className={
+                                selection === category
+                                    ? "text-black text-[0.9rem] md:text-[1rem] px-4 lg:px-8 lg:text-[1.1rem] rounded-3xl border-[1px] border-black py-2 transition-all duration-300"
+                                    : "text-black text-[0.9rem] md:text-[1rem] px-4 lg:px-8 lg:text-[1.1rem] rounded-3xl py-2"
+                            }
+                            key={idx}
+                            onClick={() => filterImages(category)}
+                        >
+                            {category}
+                        </button>
+                    );
+                })}
+            </div>
             <div className="flex flex-col items-center gap-[5rem] mb-[4rem]">
-                {workGallary.map(({ imgUrl, titleBtn, type }, idx) => {
+                {gallary.map(({ imgUrl, titleBtn, type }, idx) => {
                     return (
                         <div
-                            className="w-[95%] h-[300px] md:w-[80%] md:h-[400px] lg:w-[60%] relative flex items-end"
+                            className="w-[95%] h-[300px] md:w-[80%] md:h-[400px] relative flex items-end"
                             key={idx}
                         >
                             <div className="flex flex-col items-start justify-center gap-2 md:flex-row md:justify-between md:items-center z-50 w-full p-5">
@@ -25,6 +89,7 @@ const Gallary = () => {
                     );
                 })}
             </div>
+        </>
     );
 };
 
--- src/components/Work/Header.js
+++ src/components/Work/Header.js
@@ -4,26 +4,12 @@ import Gallary from "./Gallary";
 const Header = () => {
     return (
         <>
-            <div className="px-[5rem] mt-[3rem]">
+            <div className="px-[2rem] lg:px-[5rem] mt-[3rem]">
                 <h1 className="text-[3rem] font-semibold mb-[3rem]">
                     Our Work
                 </h1>
-                <div className="container mx-auto flex justify-center items-center gap-[2rem] mb-[4rem]">
-                    <button className="text-black rounded-3xl border-[1px] border-black py-2 px-8">
-                        All
-                    </button>
-                    <button className="text-black rounded-3xl border-[1px] border-black py-2 px-10 ">
-                        Websites
-                    </button>
-                    <button className="text-black rounded-3xl border-[1px] border-black py-2 px-10 ">
-                        Mobile Applications
-                    </button>
-                    <button className="text-black rounded-3xl border-[1px] border-black py-2 px-10 ">
-                        Branding
-                    </button>
-                </div>
+                <Gallary />
             </div>
-            <Gallary />
         </>
     );
 };

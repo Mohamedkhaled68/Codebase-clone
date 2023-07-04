60cb6bbdd96c83ae539b6877a760e646322eed7b
Mohamedkhaled68 <mohamed.khaled8ooo8@gmail.com>
2023-07-04 09:08:10 +0300

    contact us is almost done

--- src/components/ContactUs/FormSide.js
+++ src/components/ContactUs/FormSide.js
@@ -1,17 +1,23 @@
 import React from "react";
 
 const FormSide = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
     return (
         <div className="left-side flex-1">
             <h1 className="text-[3rem] font-semibold mb-[3rem]">Contact Us</h1>
-            <div className="flex flex-col items-center gap-[1rem]">
+            <form
+                onSubmit={handleSubmit}
+                className="flex flex-col items-center gap-[2rem] relative"
+            >
                 <div className="name w-full">
                     <input
                         type="text"
                         className="border-b-[1px] border-[#000] w-full py-[0.8rem] outline-none text-[1.1rem]"
                     />
                 </div>
-                <div className="emailAndPhone flex items-center gap-[1rem] w-full">
+                <div className="emailAndPhone flex items-center gap-[7rem] w-full">
                     <input
                         type="text"
                         className="border-b-[1px] border-[#000]  py-[0.8rem] outline-none text-[1.1rem] w-1/2"
@@ -27,7 +33,10 @@ const FormSide = () => {
                         className="border-b-[1px] border-[#000] w-full py-[0.8rem] outline-none text-[1.1rem]"
                     />
                 </div>
-            </div>
+                <button className="absolute right-0 bottom-[-8rem] mt-10 py-3 px-20 text-lg bg-white text-[#222] border-[#222] border-[0.7px] hover:bg-primary hover:text-white transition-colors ease-linear duration-300 rounded-full">
+                    SEND MESSAGE
+                </button>
+            </form>
         </div>
     );
 };
--- src/components/ContactUs/InformationSide.js
+++ src/components/ContactUs/InformationSide.js
@@ -10,26 +10,34 @@ import {
 const InformationSide = () => {
     return (
         <div className="right-side w-[35%]">
-            <ul className="flex flex-col gap-[3rem]">
-                <li className="w-[60%]">
+            <ul className="flex flex-col gap-[2.5rem]">
+                <li className="text-[1.3rem]">
                     Give us a call or drop by anytime, we endea vour to answer
                     all enquiries within 24 hours on all days. We will be happy
                     to answer your questions.
                 </li>
                 <li>
-                    <p>OUR MAILBOX</p>
+                    <p className="font-semibold text-primary text-[1.5rem] mb-[0.7rem]">
+                        OUR MAILBOX
+                    </p>
                     <p>info@codebase-tech.com</p>
                 </li>
                 <li>
-                    <p>OUR PHONE</p>
+                    <p className="font-semibold text-primary text-[1.5rem] mb-[0.7rem]">
+                        OUR PHONE
+                    </p>
                     <p>+201010388832</p>
                 </li>
                 <li>
-                    <p>OUR ADDRESS</p>
+                    <p className="font-semibold text-primary text-[1.5rem] mb-[0.7rem]">
+                        OUR ADDRESS
+                    </p>
                     <p>Mansoura - Egypt</p>
                 </li>
                 <li>
-                    <p className=" mb-3 font-bold">Follow us</p>
+                    <p className="uppercasee font-semibold text-primary text-[1.5rem] mb-[0.7rem]">
+                        Follow us
+                    </p>
                     <div className="icons flex items-center gap-2">
                         <FontAwesomeIcon icon={faFacebook} />
                         <FontAwesomeIcon icon={faLinkedin} />
